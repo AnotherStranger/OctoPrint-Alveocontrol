3297b1ee47caee9c1aaa902a8a4db94a4ac4fb44
AnotherStranger <AnotherStranger@users.noreply.github.com>
2022-08-02 16:59:15 +0200

    fix: :bug: add missing self

--- octoprint_alveocontrol/alveo.py
+++ octoprint_alveocontrol/alveo.py
@@ -4,17 +4,16 @@ import serial
 
 
 class AlveoController:
-    status_regex = r"\#([a-z0-9A-Z]+:?[0-9]*%?)\#"
-
     def __init__(self, port: str = "/dev/AMA0"):
         self.baudrate = 9600
         self.port = port
+        self.status_regex = r"\#([a-z0-9A-Z]+:?[0-9]*%?)\#"
 
     def _send_command(self, command: str):
         retries = 0
         while self.status != command:
             with serial.Serial(self.port, self.baudrate) as ser:
-                ser.write(bytes(command, "ascii"))
+                ser.write(bytes(f"{command};", "ascii"))
             retries += 1
             if retries > 3:
                 return
@@ -27,16 +26,16 @@ class AlveoController:
         return matches.group(1)
 
     def start(self):
-        self._send_command("start;")
+        self._send_command("start")
 
     def stop(self):
-        self._send_command("stop;")
+        self._send_command("stop")
 
     def fast(self):
-        self._send_command("fast;")
+        self._send_command("fast")
 
     def speed(self, speed: int):
         if speed > 100 or speed < 0:
             raise ValueError("Fan speed has to be between 0 and 100%.")
 
-        self._send_command(f"pwm:{speed}%;")
+        self._send_command(f"pwm:{speed}%")
